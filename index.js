5183b10c4a708f44a3f2558fc994f5a644a93566
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2020-07-08 16:20:32 +0200

    feat: multiple channles

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const sqlite3 = require("sqlite3");
 const lodash = require("lodash");
 const Sentry = require("@sentry/node");
 
+// Setup =======================================================================
+
 i18n.configure({
   defaultLocale: "ru",
   locales: ["ru", "en"],
@@ -19,77 +21,94 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
 
-const missingEnv = [
-  "BOT_TOKEN",
-  "GOOGLE_API_KEY",
-  "YOUTUBE_CHANNEL_ID",
-  "TELEGRAM_CHAT_ID",
-].filter((e) => !process.env[e]);
+const missingEnv = ["BOT_TOKEN", "GOOGLE_API_KEY"].filter(
+  (e) => !process.env[e]
+);
 
-const {
-  BOT_TOKEN,
-  GOOGLE_API_KEY,
-  YOUTUBE_CHANNEL_ID,
-  TELEGRAM_CHAT_ID,
-} = process.env;
+const { BOT_TOKEN, GOOGLE_API_KEY } = process.env;
 
 if (missingEnv.length > 0) {
   console.error("Missing ENV var:", missingEnv.join(", "));
   process.exit(1);
 }
 
-const db = new sqlite3.Database(__dirname + "/database");
-
-db.serialize(function () {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS comments (channel_id TEXT, comment_id TEXT PRIMARY KEY, timestamp INTEGER) WITHOUT ROWID"
-  );
-});
+// Main ========================================================================
 
 const telegram = new Telegram(BOT_TOKEN, {}, false);
-
-const seenComments = new Promise((resolve, reject) => {
-  db.all(
-    "SELECT * FROM comments WHERE channel_id = ? ORDER BY timestamp DESC LIMIT 5",
-    YOUTUBE_CHANNEL_ID,
-    (err, rows) => {
-      if (err) return reject(err);
-      resolve(rows.map((row) => row.comment_id));
-    }
-  );
+const youtubeApi = google.youtube({
+  version: "v3",
+  auth: GOOGLE_API_KEY,
 });
 
-const latestComments = google
-  .youtube({ version: "v3", auth: GOOGLE_API_KEY || "" })
-  .commentThreads.list({
-    part: ["snippet"],
-    allThreadsRelatedToChannelId: YOUTUBE_CHANNEL_ID,
-    moderationStatus: "published",
-    maxResults: 100,
-  })
-  .then(({ data }) => data.items);
+const db = new sqlite3.Database(__dirname + "/database");
+const dbRun = (query, params = []) =>
+  new Promise((resolve, reject) => {
+    db.run(query, params, (err) => {
+      if (err) return reject(err);
+      resolve(this);
+    });
+  });
 
-const newComments = Promise.all([seenComments, latestComments]).then(
-  takeNewComments
-);
+const dbAll = (query, params = []) =>
+  new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) return reject(err);
+      resolve(rows);
+    });
+  });
 
-const videoTitles = newComments
-  .then(fetchVideos)
-  .then((videos) =>
-    videos.reduce(
-      (acc, curr) => Object.assign(acc, { [curr.id]: curr.snippet.title }),
-      {}
-    )
+initDatabase()
+  .then(() => dbAll("SELECT * FROM youtube_channels"))
+  .then((channels) => channels.map(notifyChannel))
+  .then(() => console.log("Done"))
+  .catch((err) => console.error("error:", err))
+  .finally(() => db.close());
+
+// =============================================================================
+
+function initDatabase() {
+  return dbRun(
+    `
+      CREATE TABLE IF NOT EXISTS comments (channel_id TEXT, comment_id TEXT PRIMARY KEY, timestamp INTEGER) WITHOUT ROWID;
+      CREATE TABLE IF NOT EXISTS youtube_channels (channel_id TEXT PRIMARY KEY, telegram_chat_id TEXT, locale TEXT DEFAULT "ru") WITHOUT ROWID;
+      `
+  );
+}
+
+function notifyChannel({ channel_id, telegram_chat_id, locale }) {
+  const seenComments = dbAll(
+    "SELECT * FROM comments WHERE channel_id = ? ORDER BY timestamp DESC LIMIT 5",
+    [channel_id]
+  ).then((rows) => rows.map((row) => row.comment_id));
+
+  const latestComments = youtubeApi.commentThreads
+    .list({
+      part: ["snippet"],
+      allThreadsRelatedToChannelId: channel_id,
+      moderationStatus: "published",
+      maxResults: 100,
+    })
+    .then(({ data }) => data.items);
+
+  const newComments = Promise.all([seenComments, latestComments]).then(
+    takeNewComments(channel_id)
   );
 
-Promise.all([newComments, videoTitles])
-  .then(notify)
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+  const videoTitles = newComments
+    .then(fetchVideos)
+    .then((videos) =>
+      videos.reduce(
+        (acc, curr) => Object.assign(acc, { [curr.id]: curr.snippet.title }),
+        {}
+      )
+    );
 
-// ======================================================================
+  return Promise.all([newComments, videoTitles]).then(
+    notify(telegram_chat_id, locale)
+  );
+}
 
-function takeNewComments([seenComments, latestComments]) {
+const takeNewComments = (channelId) => ([seenComments, latestComments]) => {
   const seenCommentsSet = new Set(seenComments);
   const newComments = lodash.takeWhile(
     latestComments,
@@ -99,13 +118,13 @@ function takeNewComments([seenComments, latestComments]) {
   if (newComments.length > 0) {
     const stmt = db.prepare("INSERT INTO comments VALUES (?, ?, ?)");
     newComments.forEach(({ snippet }) => {
-      stmt.run(YOUTUBE_CHANNEL_ID, snippet.topLevelComment.id, Date.now());
+      stmt.run(channelId, snippet.topLevelComment.id, Date.now());
     });
     stmt.finalize();
   }
 
   return newComments;
-}
+};
 
 function fetchVideos(comments) {
   const videoIds = comments.map(
@@ -114,13 +133,12 @@ function fetchVideos(comments) {
 
   return videoIds.length === 0
     ? Promise.resolve([])
-    : google
-        .youtube({ version: "v3", auth: GOOGLE_API_KEY || "" })
-        .videos.list({ part: ["snippet"], id: lodash.uniq(videoIds) })
+    : youtubeApi.videos
+        .list({ part: ["snippet"], id: lodash.uniq(videoIds) })
         .then(({ data }) => data.items);
 }
 
-function notify([comments, videoTitles]) {
+const notify = (telegramChatId, locale) => ([comments, videoTitles]) => {
   const messageOptions = {
     parse_mode: "HTML",
     disable_web_page_preview: true,
@@ -128,25 +146,28 @@ function notify([comments, videoTitles]) {
 
   if (comments.length === 0) {
     return telegram.sendMessage(
-      TELEGRAM_CHAT_ID,
-      i18n.__("No new comments"),
+      telegramChatId,
+      i18n.__({ phrase: "No new comments", locale }),
       messageOptions
     );
   }
 
   const promises = comments.map(({ snippet: { topLevelComment } }, i) => {
-    const message = i18n.__("newCommentMessage", {
-      commentId: topLevelComment.id,
-      videoTitle: videoTitles[topLevelComment.snippet.videoId],
-      ...topLevelComment.snippet,
-    });
+    const message = i18n.__(
+      { phrase: "newCommentMessage", locale },
+      {
+        commentId: topLevelComment.id,
+        videoTitle: videoTitles[topLevelComment.snippet.videoId],
+        ...topLevelComment.snippet,
+      }
+    );
 
     return new Promise((resolve, reject) => {
       // Telegram API Rate Limiting workaround. 1 message per second
       setTimeout(
         () =>
           telegram
-            .sendMessage(TELEGRAM_CHAT_ID, message, messageOptions)
+            .sendMessage(telegramChatId, message, messageOptions)
             .then(resolve)
             .catch(reject),
         i * 1000
@@ -155,4 +176,4 @@ function notify([comments, videoTitles]) {
   });
 
   return Promise.all(promises);
-}
+};
