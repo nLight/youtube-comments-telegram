94254e2b4dd30f3ef179d7b51b030ad4240fd363
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2020-07-10 13:09:54 +0200

    fix: webhook, db, everything

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Telegraf } = require("telegraf");
 const i18n = require("i18n");
 const sqlite3 = require("sqlite3");
 const Sentry = require("@sentry/node");
+const { drive } = require("googleapis/build/src/apis/drive");
 
 // Setup =======================================================================
 
@@ -19,18 +20,22 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
 
-const missingEnv = ["PORT", "BOT_TOKEN", "WEBHOOK_URL"].filter(
+const missingEnv = ["PORT", "BOT_TOKEN", "DOMAIN"].filter(
   (e) => !process.env[e]
 );
 
-const { PORT, BOT_TOKEN, NODE_ENV, WEBHOOK_URL } = process.env;
+const { PORT, BOT_TOKEN, NODE_ENV, DOMAIN } = process.env;
 
 if (missingEnv.length > 0) {
   console.error("Missing ENV var:", missingEnv.join(", "));
   process.exit(1);
 }
 
-const db = new sqlite3.Database(__dirname + "/database");
+const db = new sqlite3.Database("/storage/database");
+db.run(`
+  CREATE TABLE IF NOT EXISTS comments (channel_id TEXT, comment_id TEXT PRIMARY KEY, timestamp INTEGER) WITHOUT ROWID;
+  CREATE TABLE IF NOT EXISTS youtube_channels (channel_id TEXT, telegram_chat_id TEXT, locale TEXT DEFAULT "ru", PRIMARY KEY ( channel_id, telegram_chat_id)) WITHOUT ROWID;
+`);
 
 // Main ========================================================================
 
@@ -49,8 +54,8 @@ bot.on("text", (ctx) => {
       "INSERT INTO youtube_channels (channel_id, telegram_chat_id) VALUES (?, ?)",
       [matches[1], ctx.chat.id],
       (err) => {
-        if (!err) return ctx.reply("Канал добавлен!");
-        else ctx.reply("Что-то пошло не так :(");
+        if (err) ctx.reply(`Что-то пошло не так :(\n${err.message}`);
+        else ctx.reply("Канал добавлен!");
       }
     );
   } else if (ctx.message.text === "каналы") {
@@ -58,28 +63,26 @@ bot.on("text", (ctx) => {
       "SELECT * FROM youtube_channels WHERE telegram_chat_id = ?",
       [ctx.chat.id],
       (err, rows) => {
-        if (err) return ctx.reply("Канал добавлен!");
-
-        ctx.replyWithMarkdown(
-          `Каналы подключенные к этому чату:
+        if (err) ctx.reply(`Что-то пошло не так :(\n${err.message}`);
+        else
+          ctx.replyWithMarkdown(
+            `Каналы подключенные к этому чату:
 ${rows.map((row) => `- ${row["channel_id"]}`).join("\n")}`,
-          { disable_web_page_preview: true }
-        );
+            { disable_web_page_preview: true }
+          );
       }
     );
   } else {
     ctx.replyWithMarkdown(`Я не понял команду. Попробуй:
-    - добавь [адрес канала]
-    - каналы
+- добавь [адрес канала]
+- каналы
     `);
   }
 });
 
-// bot.telegram.setWebhook(WEBHOOK_URL).catch((err) => console.log(err));
-// bot.startWebhook(BOT_TOKEN, null, parseInt(PORT, 10), "localhost");
 bot.launch({
   webhook: {
-    domain: "youtube-bot.dokku.seniorsoftwarevlogger.com",
+    domain: DOMAIN,
     port: parseInt(PORT, 10),
     cb: (req, res) => console.log(req, res),
   },
